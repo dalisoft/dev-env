1f962c64f490837241b07a102408bb178af5ac67
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-08 11:54:05 +0500

    configure tests right to work with vs-code

--- src/components/layouts/PageLayout/PageLayout.spec.jsx
+++ src/components/layouts/PageLayout/PageLayout.spec.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 import PageLayout from './PageLayout';
+
 describe('Button component', () => {
+  it('render correctly Spinner component', () => {
+    const PageLayoutComponent = create(<PageLayout>MY_CHILD_TEXT</PageLayout>);
+    expect(PageLayoutComponent).toMatchSnapshot();
+  });
+
   test('it shows the expected text when rendered', () => {
-    const component = create(<PageLayout>MY_CHILD_TEXT</PageLayout>);
-    const rootInstance = component.root;
-    expect(rootInstance.props.children).toBe('MY_CHILD_TEXT');
+    const PageLayoutComponent = create(<PageLayout>MY_CHILD_TEXT</PageLayout>);
+    const PageLayoutComponentInstance = PageLayoutComponent.root;
+    expect(PageLayoutComponentInstance.props.children).toBe('MY_CHILD_TEXT');
   });
 });
