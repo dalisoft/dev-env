349d27f352b213284da802545cd6bf0f6b002be0
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-10 20:22:00 +0500

    adding expiremental serverless support (not ready for production)

---
+++ src/instance.js
@@ -0,0 +1,31 @@
+import './env';
+import fastify from 'fastify';
+import fastifyConfig from './server-config';
+import fastifyPlugins from './fastify-plugins';
+import fastifyHandler from './errors';
+
+import { routePathsNormalizer } from './helpers';
+
+import * as appRoutes from './routes';
+import * as appMiddlewares from './middlewares';
+
+export default (serverFactory) => {
+  if (serverFactory) {
+    fastifyConfig.serverFactory = serverFactory;
+  }
+  const app = fastify(fastifyConfig);
+
+  app
+    .register(fastifyHandler)
+    .register(fastifyPlugins)
+    .register(routePathsNormalizer(appMiddlewares, appRoutes))
+    .register(routePathsNormalizer(appMiddlewares, appRoutes), {
+      prefix: '/.netlify/functions/server' // Netlify Serverless compatibility
+    });
+
+  app.get('/', async () => {
+    return { status: 'success' };
+  });
+
+  return app;
+};
--- src/server.js
+++ src/server.js
@@ -1,35 +1,19 @@
 const time = Date.now(); // For better managing start-time / lags
-
-import './env';
 import console from 'consolemd';
-import fastify from 'fastify';
-import fastifyConfig from './server-config';
-import fastifyPlugins from './fastify-plugins';
-import fastifyHandler from './errors';
 
-import { routePathsNormalizer } from './helpers';
-
-import * as appRoutes from './routes';
-import * as appMiddlewares from './middlewares';
-
-const app = fastify(fastifyConfig);
-
-app
-  .register(fastifyHandler)
-  .register(fastifyPlugins)
-  .register(routePathsNormalizer(appMiddlewares, appRoutes));
-
-app.get('/', async () => {
-  return { status: 'success' };
-});
+import app from './instance';
 
 async function start() {
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+  await app().listen(port, '0.0.0.0');
   console.log(
     `#green([*Server*]: started successfully at *localhost:${port}* in *${Date.now() -
       time}ms*)`
   );
 }
 
-start();
+if (typeof require !== 'undefined' && require.main === module) {
+  start();
+}
+
+export default app;
