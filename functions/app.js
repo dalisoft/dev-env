9bf7d67b5e220e08742ccabdffda951831fbb91a
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-10 22:44:05 +0500

    update app netlify function

--- functions/app.js
+++ functions/app.js
@@ -1,1 +1,18 @@
-module.exports = require('../lambda');
+const awsServerlessExpress = require('aws-serverless-express');
+const fastify = require('../build/server');
+
+let server;
+const serverFactory = (handler) => {
+  server = awsServerlessExpress.createServer(handler);
+  return server;
+};
+
+const app = fastify(serverFactory);
+
+exports.handler = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  app.ready((e) => {
+    if (e) return console.error(e.stack || e);
+    awsServerlessExpress.proxy(server, event, context, 'CALLBACK', callback);
+  });
+};
