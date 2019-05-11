14938e50aee5ce5c78480f4f93b44bd7e48d8cbd
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-11 06:51:57 +0500

    add expiremental cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -10,5 +10,16 @@ export default (req, res) => {
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization, Content-Length, X-Requested-With'
   );
+
+  if (req.headers && req.headers.origin) {
+    if (CORS_DOMAIN !== req.headers.origin) {
+      return res.end(
+        JSON.stringify({
+          status: 'error',
+          message: 'Declined by CORS'
+        })
+      );
+    }
+  }
   res.status(200).send({});
 };
