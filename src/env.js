02b50ee1e40c44b8f49bf089cfe58a1b0f0b321d
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-11 16:46:08 +0500

    add now and nowignore file with some fixes

--- src/env.js
+++ src/env.js
@@ -2,6 +2,14 @@ import dotenvSafe from 'dotenv-safe';
 import consolemd from 'consolemd';
 
 if (process.env.NODE_ENV === 'development') {
-  dotenvSafe.config();
-  consolemd.log('!#green([*Server*]: started in *DEVELOPMENT* mode)');
+  // Zeit now can cause fail this method
+  // So we just wrapped it within try/catch
+  // To avoid error and avoid crash of server
+  // Now.sh production/local server
+  try {
+    dotenvSafe.config();
+    consolemd.log('!#green([*Server*]: started in *DEVELOPMENT* mode)');
+  } catch (e) {
+    // Zeit now server failed
+  }
 }
