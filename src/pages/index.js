3a3a522d8fe1dcb217215dd4a418df738afb2241
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-14 02:24:39 +0500

    minor patch for file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,15 @@
-import Home from './Home';
-export default Home;
+import React from 'react';
+import { Link } from '@reach/router';
+
+import { Text } from 'components/UI';
+import { LangSwitcher } from 'components/common';
+
+export default () => (
+  <div>
+    <h1>
+      <Text>HOME_PAGE</Text>
+    </h1>
+    <LangSwitcher />
+    <Link to="/counter">Go to counter</Link>
+  </div>
+);
