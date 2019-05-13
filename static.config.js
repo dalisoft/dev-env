0ecb245c1ab74834b40bf3a5092ae769a0b887e2
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-13 23:51:06 +0500

    make config like original template

--- static.config.js
+++ static.config.js
@@ -2,17 +2,6 @@ import React from 'react';
 import path from 'path';
 
 export default {
-  target: 'web',
-  publicPath: '/',
-  webpack: (config) => {
-    /* if (config.resolve.alias.set) {
-      config.resolve.alias.set('redux', path.join(__dirname, 'src', 'redux'));
-      config.resolve.alias.set('components', path.join(__dirname, 'src', 'components'));
-      config.resolve.alias.set('pages', path.join(__dirname, 'src', 'pages'));
-      config.resolve.alias.set('modules', path.join(__dirname, 'src', 'modules'));
-      config.resolve.alias.set('helpers', path.join(__dirname, 'src', 'helpers'));
-    } */
-  },
   /* getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
@@ -34,15 +23,6 @@ export default {
       },
     ]
   },*/
-  Document: ({ Html, Head, Body, children, state: { siteData, renderMeta } }) => (
-    <Html lang="en-US">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>{children}</Body>
-    </Html>
-  ),
   plugins: [
     [
       'react-static-plugin-source-filesystem',
@@ -50,9 +30,9 @@ export default {
         location: path.resolve('./src/pages'),
       },
     ],
-    'react-static-plugin-reach-router',
-    'react-static-plugin-sitemap',
     'react-static-plugin-sass',
     'react-static-plugin-css-modules',
+    'react-static-plugin-reach-router',
+    'react-static-plugin-sitemap',
   ],
 };
