f0731473f7151aa686c03c8ef33fffa2de404501
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-13 22:30:35 +0500

    minor changes

--- static.config.js
+++ static.config.js
@@ -1,7 +1,18 @@
+import React from 'react';
 import path from 'path';
 
 export default {
   target: 'web',
+  publicPath: '/',
+  webpack: (config) => {
+    /* if (config.resolve.alias.set) {
+      config.resolve.alias.set('redux', path.join(__dirname, 'src', 'redux'));
+      config.resolve.alias.set('components', path.join(__dirname, 'src', 'components'));
+      config.resolve.alias.set('pages', path.join(__dirname, 'src', 'pages'));
+      config.resolve.alias.set('modules', path.join(__dirname, 'src', 'modules'));
+      config.resolve.alias.set('helpers', path.join(__dirname, 'src', 'helpers'));
+    } */
+  },
   /* getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
@@ -23,16 +34,25 @@ export default {
       },
     ]
   },*/
+  Document: ({ Html, Head, Body, children, state: { siteData, renderMeta } }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
   plugins: [
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      'react-static-plugin-source-filesystem',
       {
         location: path.resolve('./src/pages'),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
-    require.resolve('react-static-plugin-sass'),
-    require.resolve('react-static-plugin-css-modules'),
+    'react-static-plugin-reach-router',
+    'react-static-plugin-sitemap',
+    'react-static-plugin-sass',
+    'react-static-plugin-css-modules',
   ],
 };
