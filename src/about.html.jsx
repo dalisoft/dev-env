0a74a3fd52b5635feb445f6127ddaf0e3833319c
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-16 18:02:47 +0500

    adding data example

--- src/about.html.jsx
+++ src/about.html.jsx
@@ -1,8 +1,9 @@
 import Layout from './layout.html.jsx';
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <span>About</span>
+    <span>{JSON.stringify(data.about)}</span>
     <a href="/">Index</a>
   </Layout>
 );
