fb6e64ebc66c6dd14453563a9deed3158a80a001
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-18 21:50:29 +0500

    fix docs

--- src/wrap-el.docs.jsx
+++ src/wrap-el.docs.jsx
@@ -1,9 +1,8 @@
-import 'core-js'; // polyfills for ios 9
-import 'intl';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import "intl"
+import React from "react"
+import { Provider } from "react-redux"
+import { store } from "redux/store"
 
-const Wrap = ({ children }) => <Provider store={store}>{children}</Provider>;
+const Wrap = ({ children }) => <Provider store={store}>{children}</Provider>
 
-export default Wrap;
+export default Wrap
