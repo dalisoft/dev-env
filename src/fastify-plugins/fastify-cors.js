fdf9dd7afccd49720bded4b3ee9baa0dc511045b
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-22 10:18:59 +0500

    make cors official

--- src/fastify-plugins/fastify-cors.js
+++ src/fastify-plugins/fastify-cors.js
@@ -1,58 +1,15 @@
-import { corsWhitelist, cookie } from '../config';
-import { hasKeyObj } from '../helpers';
-import fastifyPlugin from 'fastify-plugin';
+import fastifyCors from 'fastify-cors';
+import { corsWhitelist } from '../config';
 
-export default fastifyPlugin(async (fastify) =>
-  fastify.addHook('onRequest', (req, res, next) => {
-    // eslint-disable-next-line no-unused-vars
-    const { headers, cookies, raw } = req;
-    const requestAllowedHeaders = headers['access-control-request-headers'];
-    const { origin, host, vary } = headers;
-
-    const isCredentialsInRequest = hasKeyObj(cookies) || headers.authorization;
-
-    isCredentialsInRequest &&
-      res.header('Access-Control-Allow-Credentials', 'true');
-    requestAllowedHeaders &&
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, Accept, Content-Type, X-Requested-With'
-      );
-
-    if (vary) {
-      const varyContent = [...vary.split(','), 'Origin'];
-      res.header('Vary', varyContent.join(','));
-    }
-
-    if (corsWhitelist.includes(origin)) {
-      return next();
-    }
-
-    if (origin === undefined) {
-      if (host !== undefined) {
-        const httpsOrigin = host.startsWith('https://')
-          ? host
-          : `https://${host}`;
-        const httpOrigin = host.startsWith('http://') ? host : `http://${host}`;
-
-        if (corsWhitelist.includes(httpsOrigin)) {
-          return next();
-        } else if (!cookie.secure && corsWhitelist.includes(httpOrigin)) {
-          return next();
-        }
-      }
-    }
-
-    const err = new Error(
-      `${
-        !origin
-          ? 'You trying access path with CURL or HTTP request app'
-          : 'In server your request seems untrusted'
-      }`
-    );
-    err.code = 'AccessDeniedByCORS';
-    err.statusCode = 403;
-
-    next(err);
-  })
-);
+// This plugin is official, but it's
+// not works as we except
+// so, we made own CORS plug-in
+// which more secure and faster
+export default async (fastify) =>
+  fastify.register(fastifyCors, {
+    origin: [...corsWhitelist],
+    optionsSuccessStatus: 200,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin'],
+    exposedHeaders: ['X-Admin-Token'],
+    credentials: true
+  });
