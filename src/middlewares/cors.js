e8265f399ae53caeae39ea96d45d07da055e0c94
Davlat Shavkatov <dalisoft@mail.ru>
2019-06-16 22:19:26 +0500

    improve cors, update deps

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,11 +1,18 @@
 import { CORS_DOMAIN } from '../config';
 
 export default (req, res, next) => {
-  if (req.headers && req.headers.origin) {
+  if (req.app.method === 'OPTIONS') {
+    if (!req.headers.origin || CORS_DOMAIN !== req.headers.origin) {
+      return res.status(403).json({
+        status: 'error',
+        message: 'Declined by CORS preflight request'
+      });
+    }
+  } else if (req.headers) {
     if (CORS_DOMAIN !== req.headers.origin) {
       return res.status(403).json({
         status: 'error',
-        message: 'Declined by CORS'
+        message: 'Declined by CORS HTTP request'
       });
     }
   }
