a09304887eb27dbc3a8c2c1d69611b663b98f445
Davlat Shavkatov <dalisoft@mail.ru>
2019-07-02 21:12:38 +0500

    update deps, reduce build-time, add multithread

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,24 +1,16 @@
-const nanoexpress = require('nanoexpress');
+const cluster = require('cluster');
+const os = require('os');
 
-const app = nanoexpress();
+const CLUSTER_THREAD = os.cpus().length;
 
-app.use((req, res, next) => {
-  res.writeHeader('Access-Control-Allow-Origin', 'http://localhost');
-  res.writeHeader('Access-Control-Allow-Headers', 'Content-Type, Origin');
-  res.writeHeader('Access-Control-Request-Method', '*');
-  res.writeHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  next();
-});
+if (cluster.isMaster) {
+  let i = 1;
+  while (i < CLUSTER_THREAD) {
+    cluster.fork();
+    i++;
+  }
 
-app.options('/*', (req, res) => {
-  res.writeStatus('200 OK');
-  res.end('OK');
-});
-app.get('/', (req, res) => {
-  res.send({ type: 'get', page: 'index' });
-});
-app.get('/time', (req, res) => {
-  res.send({ time: new Date().toUTCString() });
-});
-
-app.listen(process.env.PORT || 3000);
+  cluster.on('exit', () => cluster.fork());
+} else {
+  require('./worker');
+}
