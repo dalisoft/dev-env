bcb51766dd1af2a68fd78f4447317ef4a0c3d873
Davlat Shavkatov <dalisoft@mail.ru>
2019-07-08 01:17:00 +0500

    typo fix

--- src/env.js
+++ src/env.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV === 'development') {
   consolemd.log('!#green( [*Server*]: started in *DEVELOPMENT* mode )');
 
   if (!process.env.secretKey) {
-    require('dontenv-safe').config();
+    require('dotenv-safe').config();
   }
 }
